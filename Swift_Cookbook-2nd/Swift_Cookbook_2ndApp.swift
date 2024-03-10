c2bae15db8cd87e86d4c42843a17ffcd6e6089cb
Xtra IO PUrE <xoiopure@icloud.com>
2024-03-10 14:40:25 -0700

    Initial Commit

---
+++ Swift_Cookbook-2nd/Swift_Cookbook_2ndApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Cookbook_2ndApp.swift
+//  Swift_Cookbook-2nd
+//
+//  Created by Xtra Pure on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Cookbook_2ndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift_Cookbook-2ndUITests/Swift_Cookbook_2ndUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_Cookbook_2ndUITestsLaunchTests.swift
+//  Swift_Cookbook-2ndUITests
+//
+//  Created by Xtra Pure on 3/10/24.
+//
+
+import XCTest
+
+final class Swift_Cookbook_2ndUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
